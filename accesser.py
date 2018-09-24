287741a54d4f93b7988d60504cbd8909b5dda954
URenko <18209292+URenko@users.noreply.github.com>
2018-09-24 11:27:38 +0800

    add download_hosts option

--- accesser.py
+++ accesser.py
@@ -131,15 +131,19 @@ if __name__ == '__main__':
     config.read('setting.ini')
     
     if not os.path.exists('hosts'):
-        import urllib.request
-        logging.info('hosts file not exit, downloading...')
-        try:
-            local_filename, headers = urllib.request.urlretrieve(HOSTS_URL1, 'hosts')
-        except urllib.error.URLError as e:
-            logging.warning(e)
-            logging.warning('try another hosts url')
-            local_filename, headers = urllib.request.urlretrieve(HOSTS_URL2, 'hosts')
-        logging.info('saved to: '+local_filename)
+        if int(config['setting']['download_hosts']):
+            import urllib.request
+            logging.info('hosts file not exit, downloading...')
+            try:
+                local_filename, headers = urllib.request.urlretrieve(HOSTS_URL1, 'hosts')
+            except urllib.error.URLError as e:
+                logging.warning(e)
+                logging.warning('try another hosts url')
+                local_filename, headers = urllib.request.urlretrieve(HOSTS_URL2, 'hosts')
+            logging.info('saved to: '+local_filename)
+        else:
+            logging.error('No hosts file')
+            sys.exit(2)
     hosts = re.findall(r'(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\s+(\S+)', open('hosts').read())
     rhosts = {k:v for v,k in hosts}
     for domain in config['HOSTS']:
