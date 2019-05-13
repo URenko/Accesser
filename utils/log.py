6729b6c4c3ab82921e3a9793cfddb8362a019c6c
root <18209292+URenko@users.noreply.github.com>
2019-05-13 15:17:27 +0800

    limit log file size (#43)

--- utils/log.py
+++ utils/log.py
@@ -9,9 +9,14 @@ class JSONHandler(logging.handlers.QueueHandler):
     def prepare(self, record):
         return json.dumps({'level': record.levelname, 'content': self.format(record)})
 
-logging.basicConfig(filename=setting.config['log']['logfile'],
-                    format='%(asctime)s %(levelname)-8s %(name)s: %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S')
+if setting.config['log']['logfile']:
+    handlers = [logging.handlers.RotatingFileHandler(setting.config['log']['logfile'],
+                                                     maxBytes=2**20, backupCount=1)]
+else:
+    handlers = None
+logging.basicConfig(format='%(asctime)s %(levelname)-8s %(name)s: %(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S',
+                    handlers=handlers)
 logger = logging.getLogger('Accesser')
 logger.setLevel(setting.config['log']['loglevel'].upper())
 
