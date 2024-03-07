047af015c15dddf5b55601b80944068e2e7781a8
URenko <18209292+URenko@users.noreply.github.com>
2024-03-07 12:20:19 +0000

    support colorful logging if rich is installed

--- accesser/utils/log.py
+++ accesser/utils/log.py
@@ -1,5 +1,12 @@
 import logging, logging.handlers
-import json
+has_rich = True
+try:
+    from rich.logging import RichHandler
+except ModuleNotFoundError:
+    try:
+        from pip._vendor.rich.logging import RichHandler
+    except ModuleNotFoundError:
+        has_rich = False
 
 from . import setting
 
@@ -7,8 +14,8 @@ if setting.config['log']['logfile']:
     handlers = [logging.handlers.RotatingFileHandler(setting.config['log']['logfile'],
                                                      maxBytes=2**20, backupCount=1)]
 else:
-    handlers = None
-logging.basicConfig(format='%(asctime)s %(levelname)-8s %(name)s: %(message)s',
+    handlers = [RichHandler()] if has_rich else None
+logging.basicConfig(format="%(message)s" if has_rich else '%(asctime)s %(levelname)-8s %(name)s: %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S',
                     handlers=handlers)
 logger = logging.getLogger('Accesser')
