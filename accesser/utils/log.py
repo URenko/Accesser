4403d6d8c06b5fe648e0d3d12a379916cecb3544
URenko <18209292+URenko@users.noreply.github.com>
2024-03-08 07:03:43 +0000

    fix rich

--- accesser/utils/log.py
+++ accesser/utils/log.py
@@ -1,22 +1,17 @@
 import logging, logging.handlers
-has_rich = True
 try:
     from rich.logging import RichHandler
+    has_rich = True
 except ModuleNotFoundError:
-    try:
-        from pip._vendor.rich.logging import RichHandler
-    except ModuleNotFoundError:
-        has_rich = False
+    has_rich = False
 
 from . import setting
 
-if setting.config['log']['logfile']:
-    handlers = [logging.handlers.RotatingFileHandler(setting.config['log']['logfile'],
-                                                     maxBytes=2**20, backupCount=1)]
-else:
-    handlers = [RichHandler()] if has_rich else None
-logging.basicConfig(format="%(message)s" if has_rich else '%(asctime)s %(levelname)-8s %(name)s: %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S',
-                    handlers=handlers)
+is_console = not setting.config['log']['logfile']
+logging.basicConfig(
+    format="%(message)s" if has_rich and is_console else '%(asctime)s %(levelname)-8s %(name)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    handlers=[logging.handlers.RotatingFileHandler(setting.config['log']['logfile'], maxBytes=2**20, backupCount=1)] if not is_console else ([RichHandler()] if has_rich else None)
+)
 logger = logging.getLogger('Accesser')
 logger.setLevel(setting.config['log']['loglevel'].upper())
