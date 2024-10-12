c69c2f3c61b372802cfd2f097ef7830d1b73aa61
URenko <18209292+URenko@users.noreply.github.com>
2024-10-12 07:26:01 +0000

    Set up ALPN to better support HTTP/2 ( #202 )

--- accesser/__init__.py
+++ accesser/__init__.py
@@ -222,6 +222,7 @@ async def main():
     importca.import_ca()
 
     context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    context.set_alpn_protocols(['h2', 'http/1.1'])
     cert_store = set()
     cert_lock = asyncio.Lock()
     
