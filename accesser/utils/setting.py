13392f416c696da18a0925ebc44b307afded76e7
URenko <18209292+URenko@users.noreply.github.com>
2024-10-24 05:43:47 +0000

    Feature: update rules.toml automatically if it has not been modified

--- accesser/utils/setting.py
+++ accesser/utils/setting.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-import shutil
+import shutil, os, filecmp
 try:
     import tomllib
 except ModuleNotFoundError:
@@ -8,15 +8,29 @@ import argparse
 
 basepath = Path(__file__).parent.parent
 
-if not Path('rules.toml').exists():
+if Path('rules.toml').exists():
+    if Path('rules.toml').stat().st_mtime_ns > 0:
+        rules_update_case = 'modified'
+    else:
+        if filecmp.cmp('rules.toml', basepath / 'rules.toml'):
+            rules_update_case = 'holding'
+        else:
+            rules_update_case = 'old'
+else:
+    rules_update_case = 'missing'
+if rules_update_case in ('old', 'missing'):
     shutil.copyfile(basepath / 'rules.toml', 'rules.toml')
+    os.utime('rules.toml', ns=(0, 0))
 with open('rules.toml', 'rb') as f:
-    config = tomllib.load(f)
+    _rules = tomllib.load(f)
+    config = _rules.copy()
 
 if not Path('config.toml').exists():
     shutil.copyfile(basepath / 'config.toml', 'config.toml')
 with open('config.toml', 'rb') as f:
-    config |= tomllib.load(f)
+    _config = tomllib.load(f)
+
+config |= _config
 
 def parse_args():
     parser = argparse.ArgumentParser()
