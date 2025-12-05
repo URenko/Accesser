8602c3e6a50e0d51345aecca8860a59b87d70762
URenko <18209292+URenko@users.noreply.github.com>
2025-12-05 04:06:38 +0000

    v0.11.1

--- accesser/__init__.py
+++ accesser/__init__.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = '0.11.0'
+__version__ = '0.11.1'
 
 import os, sys
 import datetime
